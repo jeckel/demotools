d6172b6521c7f828121c230417ef06590892ce2e
Jeckel <julien.mercier@bureauxapartager.com>
2018-01-02 17:06:45 +0100

    Add colors and terminal tools

---
+++ src/Color.php
@@ -0,0 +1,26 @@
+<?php
+namespace DemoTools;
+
+/**
+ * Add color constants
+ */
+interface Color
+{
+    const RED         = "\033[0;31m";
+	const GREEN       = "\033[0;32m";
+	const ORANGE      = "\033[0;33m";
+	const BLUE        = "\033[0;34m";
+	const PURPLE      = "\033[0;35m";
+    const CYAN        = "\033[0;36m";
+    
+	const RED_BOLD    = "\033[0;31m\033[1m";
+	const GREEN_BOLD  = "\033[0;32m\033[1m";
+	const ORANGE_BOLD = "\033[0;33m\033[1m";
+	const BLUE_BOLD   = "\033[0;34m\033[1m";
+	const PURPLE_BOLD = "\033[0;35m\033[1m";
+    const CYAN_BOLD   = "\033[0;36m\033[1m";
+    
+	const NC          = "\033[0m"; # No Color
+	const BOLD        = "\033[1m";
+    const NEGATIV     = "\033[7m";
+}
---
+++ src/Terminal.php
@@ -0,0 +1,109 @@
+<?php
+namespace DemoTools;
+
+/**
+ * Terminal tools
+ */
+class Terminal
+{
+    const DFL_TITLE_COLOR = Color::BLUE_BOLD;
+
+    const DFL_SUCCESS_COLOR = Color::GREEN;
+
+    const DFL_FAIILURE_COLOR = Color::RED_BOLD;
+
+    /**
+     * Clear screen
+     */
+    static public function cls()
+    {
+        system('clear');
+    }
+
+    /**
+     * Clear screen
+     * @alias cls
+     */
+    static public function clear()
+    {
+        self::cls();
+    }
+
+    /**
+     * Pause, wait for a key pressed by the user
+     */
+    static public function pause()
+    {
+        system('read -n1 -r -p "Press any key to continue..."');
+    }
+
+    /**
+     * Read user input
+     *
+     * @param string|null $message
+     * @return string
+     */
+    static public function readUserEntry(string $message = null): string
+    {
+        if (! empty($message)) {
+            printf($message);
+        }
+        return trim(fgets(STDIN));
+    }
+
+    /**
+     * Print title
+     * @param string $title
+     */
+    static public function printTitle(string $title, string $color = self::DFL_TITLE_COLOR)
+    {
+        printf($color . $title . Color::NC . "\n");
+    }
+
+    /**
+     * @param array $items
+     * @param string $title
+     * @return string
+     */
+    static public function menu(array $items, string $title = "Select an iteml"): string
+    {
+        self::clear();
+        self::printTitle($title);
+        foreach($items as $key => $item) {
+            printf (" - [%s] %s\n", $key, $item);
+        }
+        $selected = -1;
+        while(! in_array($selected, array_keys($items))) {
+            $selected = self::readUserEntry("Your choice: ");
+        }
+        return $selected;
+    }
+
+    /**
+     * Print colored line
+     * @param string $message
+     * @param string $color
+     */
+    static public function printColoredLine(string $message, string $color)
+    {
+        printf($color . $message . Color::NC . "\n");
+    }
+
+    /**
+     * @param string $success
+     * @param string $color
+     */
+    static public function printSuccess(string $success = 'success', string $color = self::DFL_SUCCESS_COLOR)
+    {
+        self::printColoredLine($success, $color);
+    }
+
+    /**
+     * @param string $failure
+     * @param string $color
+     */
+    static public function printFailure(string $failure = 'failure', string $color = self::DFL_FAIILURE_COLOR)
+    {
+        self::printColoredLine($failure, $color);
+    }
+}
